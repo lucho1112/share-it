655c2abdf6aec1f33eae5951ea791a180752de87
lucho1112 <lucas.riveill@gmail.com>
2020-05-19 12:13:08 +0200

    created NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 import Main from "./containers/Main/Main";
+import NavBar from "./components/NavBar/NavBar";
 import "./App.scss";
 
 const App = () => {
-  return <Main />;
+  return (
+    <div>
+      <NavBar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
